17c36686a3375ce4bcee18631a72fdb7e92c1fce
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2024-03-19 11:53:45 -0700

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+THEME_COLOR = "#375362"
+Q_TEXT_FONT = ("Arial", 20, "italic")
+
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.question_text = self.canvas.create_text(150, 125, text="Sample text.", fill="black", font=Q_TEXT_FONT)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
